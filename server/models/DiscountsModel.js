b35117957aded954586603e9ca2c893389f21bb5
tan nguyen <nguyen.tan.engr@gmail.com>
2025-06-17 23:24:57 +0700

    feature | payment api

--- server/models/DiscountsModel.js
+++ server/models/DiscountsModel.js
@@ -1,18 +1,18 @@
+// models/discount.model.js
 const mongoose = require('mongoose');
-const {normalizeVieText} = require("../utils/normalize");
 
 const discountSchema = new mongoose.Schema({
     name: { type: String, required: true },
     description: { type: String, required: true },
-    min_people: { type: Number, required: true },
-    discount_percentage: { type: Number },
-    discount_amount: { type: Number },
-    tour_ids: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tour' }],
-    start_date: { type: Date, required: true },
-    end_date: { type: Date, required: true },
+    minPeople: { type: Number, required: true },
+    discountPercentage: { type: Number },
+    discountAmount: { type: Number },
+    tourIds: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tour' }],
+    startDate: { type: Date, required: true },
+    endDate: { type: Date, required: true },
     status: { type: String, required: true },
-    created_by: {
-        admin_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Admin', required: true },
+    createdBy: {
+        adminId: { type: mongoose.Schema.Types.ObjectId, ref: 'Admin', required: true },
         name: { type: String, required: true }
     },
     createdAt: { type: Date, default: Date.now },
@@ -20,6 +20,6 @@ const discountSchema = new mongoose.Schema({
     deletedAt: { type: Date, default: null }
 });
 
-discountSchema.index({ name: 1 });
+discountSchema.index({ name: 'text' });
 
 module.exports = mongoose.model('Discount', discountSchema);
