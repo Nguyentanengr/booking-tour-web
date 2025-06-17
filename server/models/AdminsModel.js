44930a0000cf53c06aa60c2c1bccc1b2621f4a2c
Kietptk2k4 <Kietpt301104@gmail.com>
2025-06-18 03:20:20 +0700

    sau lai dang ky

--- server/models/AdminsModel.js
+++ server/models/AdminsModel.js
@@ -1,5 +1,6 @@
 // models/admin.model.js
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const adminSchema = new mongoose.Schema({
     fullName: { type: String, required: true },
@@ -17,5 +18,16 @@ const adminSchema = new mongoose.Schema({
 });
 
 adminSchema.index({ fullName: 'text' });
+
+adminSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+adminSchema.methods.comparePassword = async function (candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password);
+};
 
 module.exports = mongoose.model('Admin', adminSchema);
--- server/models/UsersModel.js
+++ server/models/UsersModel.js
@@ -1,5 +1,6 @@
 // models/user.model.js
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     fullName: { type: String, required: true },
@@ -25,5 +26,18 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.index({ fullName: 'text' });
+
+// Hook để hash mật khẩu
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+// Phương thức so sánh mật khẩu
+userSchema.methods.comparePassword = async function (candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password);
+};
 
 module.exports = mongoose.model('User', userSchema);
